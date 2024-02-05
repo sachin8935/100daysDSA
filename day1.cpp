8568f54782a2790697732679e8165d408e041365
Sachin Kumar <sachin89359@gmail.com>
2024-02-05 13:43:15 +0530

    day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,4 @@
+Q1: https://leetcode.com/problems/first-unique-character-in-a-string/submissions/1166579700/?envType=daily-question&envId=2024-02-05
 class Solution {
 public:
     int firstUniqChar(string s) {
